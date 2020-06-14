8be51313c601e6685553b11ac127d2c40b4a088d
Jason Lampel <jason@abettermap.com>
2020-06-14 12:40:02 -0600

    Init. polygon style for state income GeoJSON example

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,21 +1,38 @@
-import React, { FC, useState } from 'react'
-import MapGL from 'react-map-gl'
+import React, { FC, useState, useEffect } from 'react'
+import MapGL, { Source, Layer } from 'react-map-gl'
 
 import { MAPBOX_TOKEN as mapboxApiAccessToken } from 'config'
+import { dataLayer } from './map-style'
 
 export const Map: FC = () => {
   // Unsure why it needs the type here but not for feature coords...
-  const hood = [-74.006, 40.7128] as [number, number]
-  const zoom = 13
-  const bearing = -13
-  const pitch = 45
+  const nyc = [-74.006, 40.7128] as [number, number]
+  const zoom = 4
+  // TODO: restore when makes sense to
+  // const bearing = -13
+  // const pitch = 45
+  const [data, setData] = useState()
   const [viewport, setViewport] = useState({
-    latitude: hood[1],
-    longitude: hood[0],
+    latitude: nyc[1],
+    longitude: nyc[0],
     zoom,
-    bearing,
-    pitch,
+    // TODO: restore when makes sense to
+    // bearing,
+    // pitch,
   })
+
+  useEffect(() => {
+    async function anyNameFunction() {
+      const URL =
+        'https://raw.githubusercontent.com/uber/react-map-gl/master/examples/.data/us-income.geojson'
+      const response = await fetch(URL)
+      const geojson = await response.json()
+      setData(geojson)
+    }
+
+    // Execute the created function directly
+    anyNameFunction()
+  }, [])
 
   return (
     <MapGL
@@ -25,6 +42,12 @@ export const Map: FC = () => {
       mapStyle="mapbox://styles/mapbox/dark-v9"
       onViewportChange={setViewport}
       mapboxApiAccessToken={mapboxApiAccessToken}
-    />
+    >
+      {data && (
+        <Source type="geojson" data={data}>
+          <Layer {...dataLayer} />
+        </Source>
+      )}
+    </MapGL>
   )
 }
---
+++ src/components/map/map-style.ts
@@ -0,0 +1,10 @@
+// For more information on data-driven styles, see
+// https://www.mapbox.com/help/gl-dds-ref/
+export const dataLayer = {
+  id: 'data',
+  type: 'fill',
+  paint: {
+    'fill-color': 'green',
+    'fill-opacity': 0.8,
+  },
+}
