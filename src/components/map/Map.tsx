7060bc712441fdeb6634dfc2b715b857b8a1329e
Jason Lampel <jason@abettermap.com>
2020-06-15 12:29:39 -0600

    Map: add (outdated) language points from ArcGIS Service

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -2,16 +2,17 @@ import React, { FC, useState, useEffect } from 'react'
 import MapGL, { Source, Layer } from 'react-map-gl'
 
 import { MAPBOX_TOKEN as mapboxApiAccessToken } from 'config'
-import { dataLayer } from './map-style'
+import { polygonStyle, pointStyle } from './map-style'
 
 export const Map: FC = () => {
   // Unsure why it needs the type here but not for feature coords...
-  const nyc = [-74.006, 40.7128] as [number, number]
-  const zoom = 4
+  const nyc = [-73.96, 40.7128] as [number, number]
+  const zoom = 10
   // TODO: restore when makes sense to
   // const bearing = -13
   // const pitch = 45
-  const [data, setData] = useState()
+  const [polygonData, setPolygonData] = useState()
+  const [pointData, setPointData] = useState()
   const [viewport, setViewport] = useState({
     latitude: nyc[1],
     longitude: nyc[0],
@@ -22,16 +23,26 @@ export const Map: FC = () => {
   })
 
   useEffect(() => {
-    async function anyNameFunction() {
+    async function getPolygons() {
       const URL =
         'https://raw.githubusercontent.com/uber/react-map-gl/master/examples/.data/us-income.geojson'
       const response = await fetch(URL)
       const geojson = await response.json()
-      setData(geojson)
+
+      setPolygonData(geojson)
     }
 
-    // Execute the created function directly
-    anyNameFunction()
+    async function getPoints() {
+      const URL =
+        'https://services2.arcgis.com/NlsizNmbMFiinWw4/ArcGIS/rest/services/Languages_of_New_York_test/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
+      const response = await fetch(URL)
+      const geojson = await response.json()
+
+      setPointData(geojson)
+    }
+
+    getPolygons()
+    getPoints()
   }, [])
 
   return (
@@ -43,9 +54,14 @@ export const Map: FC = () => {
       onViewportChange={setViewport}
       mapboxApiAccessToken={mapboxApiAccessToken}
     >
-      {data && (
-        <Source type="geojson" data={data}>
-          <Layer {...dataLayer} />
+      {pointData && (
+        <Source type="geojson" data={pointData}>
+          <Layer {...pointStyle} />
+        </Source>
+      )}
+      {polygonData && (
+        <Source type="geojson" data={polygonData}>
+          <Layer {...polygonStyle} />
         </Source>
       )}
     </MapGL>
--- src/components/map/map-style.ts
+++ src/components/map/map-style.ts
@@ -1,10 +1,19 @@
 // For more information on data-driven styles, see
 // https://www.mapbox.com/help/gl-dds-ref/
-export const dataLayer = {
-  id: 'data',
+export const polygonStyle = {
+  id: 'polygons',
   type: 'fill',
   paint: {
     'fill-color': 'green',
-    'fill-opacity': 0.8,
+    'fill-opacity': 0.6,
+  },
+}
+
+export const pointStyle = {
+  id: 'points',
+  type: 'circle',
+  paint: {
+    'circle-radius': 6,
+    'circle-color': '#007cbf',
   },
 }
