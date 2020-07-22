3777654073cf8dcd7d784197febf400f4ae92549
Jason Lampel <jason@abettermap.com>
2020-07-22 13:34:52 -0400

    Popups: simplify and downsize

--- src/components/map/MapPopup.tsx
+++ src/components/map/MapPopup.tsx
@@ -1,13 +1,9 @@
 import React, { FC } from 'react'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import queryString from 'query-string'
-import { Link as RouteLink } from 'react-router-dom'
 import { Popup } from 'react-map-gl'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import { Typography, Divider } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
 import { LongLatType } from './types'
-import { LangRecordSchema, ActivePanelRouteType } from '../../context/types'
+import { LangRecordSchema } from '../../context/types'
 
 type PopupComponentType = LongLatType & {
   popupOpen: boolean
@@ -19,19 +15,20 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       textAlign: 'center',
-      width: 250,
-      [theme.breakpoints.up('sm')]: {
-        width: 300,
-      },
+      width: 200,
       '& a[role="button"]': {
         color: theme.palette.common.white,
       },
+      '& .mapboxgl-popup-content': {
+        padding: theme.spacing(1),
+      },
     },
-    intro: {
-      paddingBottom: theme.spacing(1),
+    heading: {
+      lineHeight: 1.2,
     },
     subHeading: {
       fontSize: theme.typography.caption.fontSize,
+      fontStyle: 'italic',
     },
   })
 )
@@ -41,15 +38,11 @@ export const MapPopup: FC<PopupComponentType> = ({
   latitude,
   setPopupOpen,
   popupAttribs,
-  popupOpen,
 }) => {
   const classes = useStyles()
-  const baseRoute: ActivePanelRouteType = '/details'
-  const parsed = queryString.parse(window.location.search)
-  const strung = {
-    ...parsed,
-    id: popupAttribs.ID,
-  }
+
+  // NOTE: the longest legit language or endonym so far is 27 characters:
+  // English, Cameroonian Pidgin
 
   return (
     <Popup
@@ -61,27 +54,16 @@ export const MapPopup: FC<PopupComponentType> = ({
       className={classes.root}
       onClose={() => setPopupOpen(false)}
     >
-      <header className={classes.intro}>
-        <Typography variant="h4" component="h3">
+      <header>
+        <Typography variant="h6" component="h3" className={classes.heading}>
           {popupAttribs['Language Endonym' || 'Language']}
         </Typography>
         {popupAttribs['NYC Neighborhood'] && (
-          <Typography className={classes.subHeading}>
-            <i>Neighborhood: {popupAttribs['NYC Neighborhood']}</i>
-          </Typography>
+          <small className={classes.subHeading}>
+            {popupAttribs['NYC Neighborhood']}
+          </small>
         )}
       </header>
-      <Divider variant="middle" />
-      <p>
-        <RouteLink
-          to={`${baseRoute}?${queryString.stringify(strung)}`}
-          onClick={() => {
-            setPopupOpen(false)
-          }}
-        >
-          View Details
-        </RouteLink>
-      </p>
     </Popup>
   )
 }
