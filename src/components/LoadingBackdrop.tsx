71451bdeb6306ce76d2fbd3c84d9356cb39507b6
Jason Lampel <jason@abettermap.com>
2020-08-11 20:17:12 -0600

    Loading backdrop: improve style, and position on mobile

--- src/components/LoadingBackdrop.tsx
+++ src/components/LoadingBackdrop.tsx
@@ -5,11 +5,20 @@ import { Backdrop, CircularProgress, Typography } from '@material-ui/core'
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     backdropRoot: {
-      zIndex: theme.zIndex.drawer + 1,
       color: '#fff',
       flexDirection: 'column',
+      textAlign: 'center',
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    backdropContent: {
+      [theme.breakpoints.down('sm')]: {
+        position: 'absolute',
+        height: '100%',
+        top: '25%',
+      },
     },
     text: {
+      textShadow: '1px 1px 5px #444',
       marginBottom: theme.spacing(1),
     },
   })
@@ -20,10 +29,12 @@ export const LoadingBackdrop: FC = () => {
 
   return (
     <Backdrop className={classes.backdropRoot} open>
-      <Typography variant="h4" component="h2" className={classes.text}>
-        Loading...
-      </Typography>
-      <CircularProgress color="inherit" />
+      <div className={classes.backdropContent}>
+        <Typography variant="h4" component="h2" className={classes.text}>
+          Loading...
+        </Typography>
+        <CircularProgress color="inherit" size={38} />
+      </div>
     </Backdrop>
   )
 }
