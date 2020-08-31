Language-Mapping/language-map

commit a2c9a93e9939ecd95474da9f1f89c2f4e0b6d98f

Geocoder: zoom to polygon extent; cleanup

src/components/map/GeocoderPopout.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react'
+import { Map } from 'mapbox-gl'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { Typography, Popover, Box } from '@material-ui/core'
 import Geocoder from 'react-map-gl-geocoder'
 
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
-import { flyToCoords } from './utils'
+import { flyToCoords, getWebMercSettings } from './utils'
 import { MapCtrlBtnsProps, GeocodeResult } from './types'
 import { MAPBOX_TOKEN, NYC_LAT_LONG } from './config'
+import { useWindowResize } from '../../utils'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -39,32 +41,55 @@ const LocationSearchContent: FC = (props) => {
   )
 }
 
-type GeocoderProps = Pick<MapCtrlBtnsProps, 'mapRef' | 'mapOffset'> & {
+type GeocoderProps = Omit<MapCtrlBtnsProps, 'onMapCtrlClick'> & {
   anchorEl: null | HTMLElement
   setAnchorEl: React.Dispatch<null | HTMLElement>
 }
 
 export const GeocoderPopout: FC<GeocoderProps> = (props) => {
-  const { anchorEl, setAnchorEl, mapOffset, mapRef } = props
+  const { anchorEl, setAnchorEl, mapOffset, mapRef, isDesktop } = props
   const classes = useStyles()
   const layersMenuOpen = Boolean(anchorEl)
   const geocoderContainerRef = React.useRef<HTMLDivElement>(null)
+  const { width, height } = useWindowResize()
 
   const handleLayersMenuClose = () => setAnchorEl(null)
 
   const handleGeocodeResult = (geocodeResult: GeocodeResult) => {
     handleLayersMenuClose()
 
-    if (mapRef.current) {
+    if (!mapRef.current) return
+
+    const { center, bbox } = geocodeResult.result
+
+    if (bbox) {
+      const { latitude, longitude, zoom } = getWebMercSettings(
+        width,
+        height,
+        isDesktop,
+        mapOffset,
+        [
+          [bbox[0], bbox[1]],
+          [bbox[2], bbox[3]],
+        ]
+      )
+
+      const map: Map = mapRef.current.getMap()
+
+      map.flyTo(
+        {
+          // Not THAT essential if you... don't like cool things
+          essential: true,
+          center: { lng: longitude, lat: latitude },
+          zoom,
+        },
+        { forceViewportUpdate: true }
+      )
+    } else {
       flyToCoords(
         mapRef.current.getMap(),
-        {
-          latitude: geocodeResult.result.center[1],
-          longitude: geocodeResult.result.center[0],
-          zoom: 15, // TODO: bounds
-        },
-        mapOffset,
-        null
+        { latitude: center[1], longitude: center[0], zoom: 15 },
+        mapOffset
       )
     }
   }

src/components/map/Map.tsx
@@ -116,16 +116,11 @@ export const Map: FC<MapTypes.MapComponent> = ({
     setPopupOpen(null)
     setSelFeatState(map, ID, true)
 
-    utils.flyToCoords(
-      map,
-      { latitude, longitude, zoom: 12 },
-      mapOffset,
-      selFeatAttribs
-    )
+    utils.flyToCoords(map, { latitude, longitude, zoom: 12 }, mapOffset)
   }, [selFeatAttribs, mapLoaded])
   /* eslint-enable react-hooks/exhaustive-deps */
 
-  // TODO: animate selected feature
+  // TODO: higher zIndex on selected feature
   function setSelFeatState(map: MbMap, id: number, selected: boolean) {
     map.setFeatureState(
       { sourceLayer, source: internalSrcID, id },
@@ -157,7 +152,7 @@ export const Map: FC<MapTypes.MapComponent> = ({
     })
 
     map.on('zoomend', function onMoveEnd(zoomEndEvent) {
-      if (zoomEndEvent.selFeatAttribs && !map.isMoving()) {
+      if (selFeatAttribs && !map.isMoving()) {
         setPopupOpen({
           latitude: zoomEndEvent.selFeatAttribs.Latitude,
           longitude: zoomEndEvent.selFeatAttribs.Longitude,
@@ -249,12 +244,14 @@ export const Map: FC<MapTypes.MapComponent> = ({
       width,
       height,
       isDesktop,
-      mapOffset
+      mapOffset,
+      config.initialBounds
     )
 
     // Don't really need the `flyToCoords` util for this first one
     map.flyTo(
       {
+        essential: true, // not THAT essential if you... don't like cool things
         center: { lng: longitude, lat: latitude },
         zoom,
       },
@@ -284,18 +281,16 @@ export const Map: FC<MapTypes.MapComponent> = ({
       return // assumes `in` or `out` from here down
     }
 
-    const { zoom, latitude, longitude } = viewport
+    const { zoom } = viewport
 
     utils.flyToCoords(
       map,
       {
-        latitude,
-        longitude,
+        ...viewport,
         zoom: actionID === 'in' ? zoom + 1 : zoom - 1,
         disregardCurrZoom: true,
       },
-      [0, 0],
-      selFeatAttribs
+      [0, 0]
     )
   }
 

src/components/map/types.ts
@@ -109,8 +109,7 @@ export type FlyToCoords = (
     zoom?: number | 10.25
     disregardCurrZoom?: boolean // e.g. when using map controls
   } & LongLat,
-  offset: [number, number],
-  selFeatAttribs: LangRecordSchema | null
+  offset: [number, number]
 ) => void
 
 export type UseStyleProps = {
@@ -119,7 +118,10 @@ export type UseStyleProps = {
 }
 
 export type GeocodeResult = {
-  result: { center: [number, number] }
+  result: {
+    center: [number, number]
+    bbox?: [number, number, number, number]
+  }
 }
 
 export type MapCtrlBtnsProps = {
@@ -135,3 +137,5 @@ export type CtrlBtnConfig = {
   name: string
   customFn?: boolean
 }
+
+export type BoundsArray = [[number, number], [number, number]]

src/components/map/utils.ts
@@ -3,7 +3,6 @@ import { WebMercatorViewport } from 'react-map-gl'
 
 import { PAGE_HEADER_ID } from 'components/nav/config'
 import * as MapTypes from './types'
-import { initialBounds } from './config'
 import { isURL } from '../../utils'
 
 // One of the problems of using panels which overlap the map is how to deal with
@@ -35,12 +34,7 @@ export const prepMapOffset = (
   return [(sidePanelWidth + sidePanelGutter) / 2, topBarHeight / 2]
 }
 
-export const flyToCoords: MapTypes.FlyToCoords = (
-  map,
-  settings,
-  offset,
-  selFeatAttribs
-) => {
+export const flyToCoords: MapTypes.FlyToCoords = (map, settings, offset) => {
   const {
     zoom: targetZoom,
     latitude: lat,
@@ -49,9 +43,8 @@ export const flyToCoords: MapTypes.FlyToCoords = (
   } = settings
   const currentZoom = map.getZoom()
   const customEventData = {
-    forceViewportUpdate: true, // to keep state in sync
-    selFeatAttribs, // popup data
     disregardCurrZoom,
+    forceViewportUpdate: true, // to keep state in sync
   }
   let zoomToUse = targetZoom
 
@@ -62,6 +55,7 @@ export const flyToCoords: MapTypes.FlyToCoords = (
 
   map.flyTo(
     {
+      essential: true, // not THAT essential if you... don't like cool things
       center: { lng, lat },
       offset,
       zoom: zoomToUse,
@@ -160,12 +154,13 @@ export const getWebMercSettings = (
   width: number,
   height: number,
   isDesktop: boolean,
-  mapOffset: [number, number]
+  mapOffset: [number, number],
+  bounds: MapTypes.BoundsArray
 ): { latitude: number; longitude: number; zoom: number } => {
   return new WebMercatorViewport({
     width,
     height,
-  }).fitBounds(initialBounds, {
+  }).fitBounds(bounds, {
     padding: {
       bottom: isDesktop ? mapOffset[1] : height / 2,
       left: isDesktop ? mapOffset[0] : 0,

src/components/map/MapCtrlBtns.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import { Typography, Box } from '@material-ui/core'
 import {
   SpeedDial,
   SpeedDialIcon,
@@ -40,24 +39,6 @@ const useStyles = makeStyles((theme: Theme) =>
         },
       },
     },
-    popoverContent: {
-      padding: '1em',
-      width: 300,
-    },
-    popoverContentHeading: {
-      marginTop: '.5rem',
-    },
-    popoverContentText: {
-      marginBottom: '.75em',
-      fontSize: '0.8em',
-    },
-    layersMenuPaper: {
-      overflow: 'visible',
-    },
-    locationBtnWrap: {
-      display: 'flex',
-      justifyContent: 'center',
-    },
   })
 )
 
@@ -73,28 +54,6 @@ const ctrlBtnsConfig = [
   },
   { id: 'info', icon: <FiInfo />, name: 'About & Info' },
 ] as CtrlBtnConfig[]
-
-export const LocationSearchContent: FC = (props) => {
-  const { children } = props
-  const classes = useStyles()
-
-  return (
-    <Box className={classes.popoverContent}>
-      <Typography variant="h5" component="h3">
-        Search by location
-      </Typography>
-      <Typography className={classes.popoverContentText}>
-        <small>
-          Enter an address, municipality, neighborhood, postal code, landmark,
-          or other point of interest. Note that this project's focus is on the
-          New York City metro area and surrounding locations, so no communities
-          will be found outside that extent.
-        </small>
-      </Typography>
-      {children}
-    </Box>
-  )
-}
 
 export const MapCtrlBtns: FC<MapCtrlBtnsProps> = (props) => {
   const { isDesktop, onMapCtrlClick, mapRef, mapOffset } = props
@@ -121,7 +80,9 @@ export const MapCtrlBtns: FC<MapCtrlBtnsProps> = (props) => {
 
   return (
     <>
-      <GeocoderPopout {...{ anchorEl, setAnchorEl, mapOffset, mapRef }} />
+      <GeocoderPopout
+        {...{ anchorEl, setAnchorEl, mapOffset, mapRef, isDesktop }}
+      />
       <SpeedDial
         ariaLabel="Map control buttons"
         className={classes.mapCtrlsRoot}

src/components/map/config.ts
@@ -1,5 +1,6 @@
 // TODO: rm if only using local, otherwise restore when ready
 // const MB_STYLES_API_URL = 'https://api.mapbox.com/styles/v1'
+import { BoundsArray } from './types'
 
 import iconTree from './icons/tree.svg'
 import iconBook from './icons/book.svg'
@@ -50,7 +51,7 @@ export const initialMapState = {
 export const initialBounds = [
   [-74.19564, 40.574533],
   [-73.767185, 40.892251],
-] as [[number, number], [number, number]]
+] as BoundsArray
 
 export const langTypeIconsConfig = [
   { icon: iconTree, id: '_tree' },

commit efc8b8e7888b084019a0fb7e9d144ec023c5269f

Map popups: restore (broken by a2c9a93), consistentize behavior for geocoder:
Close the popup on `movestart` in all instances including geocoder
search to avoid the janky feel of popup staying open while moving.

src/components/map/GeocoderPopout.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { Map } from 'mapbox-gl'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { Typography, Popover, Box } from '@material-ui/core'
@@ -6,6 +6,7 @@ import Geocoder from 'react-map-gl-geocoder'
 
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
+import { GlobalContext } from 'components'
 import { flyToCoords, getWebMercSettings } from './utils'
 import { MapCtrlBtnsProps, GeocodeResult } from './types'
 import { MAPBOX_TOKEN, NYC_LAT_LONG } from './config'
@@ -45,6 +46,7 @@ type GeocoderProps = Omit<MapCtrlBtnsProps, 'onMapCtrlClick'> & {
 }
 
 export const GeocoderPopout: FC<GeocoderProps> = (props) => {
+  const { state } = useContext(GlobalContext)
   const { anchorEl, setAnchorEl, mapOffset, mapRef, isDesktop } = props
   const classes = useStyles()
   const layersMenuOpen = Boolean(anchorEl)
@@ -81,13 +83,18 @@ export const GeocoderPopout: FC<GeocoderProps> = (props) => {
           center: { lng: longitude, lat: latitude },
           zoom,
         },
-        { forceViewportUpdate: true }
+        {
+          forceViewportUpdate: true,
+          selFeatAttribs: state.selFeatAttribs,
+          fromPoly: true,
+        }
       )
     } else {
       flyToCoords(
         mapRef.current.getMap(),
         { latitude: center[1], longitude: center[0], zoom: 15 },
-        mapOffset
+        mapOffset,
+        state.selFeatAttribs
       )
     }
   }

src/components/map/Map.tsx
@@ -113,10 +113,14 @@ export const Map: FC<MapTypes.MapComponent> = ({
 
     const { ID, Latitude: latitude, Longitude: longitude } = selFeatAttribs
 
-    setPopupOpen(null)
     setSelFeatState(map, ID, true)
 
-    utils.flyToCoords(map, { latitude, longitude, zoom: 12 }, mapOffset)
+    utils.flyToCoords(
+      map,
+      { latitude, longitude, zoom: 12 },
+      mapOffset,
+      selFeatAttribs
+    )
   }, [selFeatAttribs, mapLoaded])
   /* eslint-enable react-hooks/exhaustive-deps */
 
@@ -136,31 +140,6 @@ export const Map: FC<MapTypes.MapComponent> = ({
   function onLoad(mapLoadEvent: MapLoadEvent) {
     const { target: map } = mapLoadEvent
 
-    // Maintain viewport state sync if needed (e.g. after things like `flyTo`),
-    // otherwise the map shifts back to previous position after panning or
-    // zooming.
-    map.on('moveend', function onMoveEnd(zoomEndEvent) {
-      // No custom event data, regular move event
-      if (zoomEndEvent.forceViewportUpdate) {
-        setViewport({
-          ...viewport, // spreading just in case bearing or pitch are added
-          zoom: map.getZoom(),
-          latitude: map.getCenter().lat,
-          longitude: map.getCenter().lng,
-        })
-      }
-    })
-
-    map.on('zoomend', function onMoveEnd(zoomEndEvent) {
-      if (selFeatAttribs && !map.isMoving()) {
-        setPopupOpen({
-          latitude: zoomEndEvent.selFeatAttribs.Latitude,
-          longitude: zoomEndEvent.selFeatAttribs.Longitude,
-          selFeatAttribs: zoomEndEvent.selFeatAttribs,
-        })
-      }
-    })
-
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const langSrcBounds = map.getSource('languages-src').bounds
@@ -173,7 +152,6 @@ export const Map: FC<MapTypes.MapComponent> = ({
     const cacheOfIDs: number[] = []
     const uniqueRecords: LangRecordSchema[] = []
     const rawLangFeats = map.querySourceFeatures(internalSrcID, { sourceLayer })
-
     // Just the properties for table/results, don't need GeoJSON cruft. Also
     // need to make sure each ID is unique as there have been initial data
     // inconsistencies, and more importantly MB may have feature duplication if
@@ -205,6 +183,38 @@ export const Map: FC<MapTypes.MapComponent> = ({
     dispatch({ type: 'SET_MAP_LOADED', payload: true })
 
     map.addControl(new AttributionControl({ compact: false }), 'bottom-right')
+
+    // Maintain viewport state sync if needed (e.g. after things like `flyTo`),
+    // otherwise the map shifts back to previous position after panning or
+    // zooming.
+    map.on('moveend', function onMoveEnd(zoomEndEvent) {
+      // No custom event data, regular move event
+      if (zoomEndEvent.forceViewportUpdate) {
+        setViewport({
+          ...viewport, // spreading just in case bearing or pitch are added
+          zoom: map.getZoom(),
+          latitude: map.getCenter().lat,
+          longitude: map.getCenter().lng,
+        })
+      }
+    })
+
+    // Close popup on the start of moving so no jank
+    map.on('movestart', function onMoveStart(zoomEndEvent) {
+      if (zoomEndEvent.selFeatAttribs) setPopupOpen(null)
+    })
+
+    map.on('zoomend', function onMoveEnd(zoomEndEvent) {
+      const { selFeatAttribs: attribs } = zoomEndEvent
+
+      if (attribs && !map.isMoving()) {
+        setPopupOpen({
+          latitude: attribs.Latitude,
+          longitude: attribs.Longitude,
+          selFeatAttribs: attribs,
+        })
+      }
+    })
   }
 
   function onNativeClick(event: MapTypes.MapEvent): void {
@@ -223,8 +233,9 @@ export const Map: FC<MapTypes.MapComponent> = ({
 
       return
     }
+
     setTooltipOpen(null) // super annoying if tooltip stays intact after a click
-    setPopupOpen(null)
+    setPopupOpen(null) // closed by movestate anyway, but smoother this way
 
     // TODO: use `initialEntries` in <MemoryRouter> to test routing
     history.push(`/details?id=${event.features[0].properties.ID}`)
@@ -267,31 +278,25 @@ export const Map: FC<MapTypes.MapComponent> = ({
       dispatch({
         type: 'TOGGLE_OFF_CANVAS_NAV',
       })
-
-      return
-    }
-
-    const map: MbMap = mapRef.current.getMap()
-
-    setPopupOpen(null) // otherwise janky lag while map is moving
-
-    if (actionID === 'home') {
+    } else if (actionID === 'home') {
       flyHome()
+    } else {
+      // Assumes `in` or `out` from here down...
 
-      return // assumes `in` or `out` from here down
+      const map: MbMap = mapRef.current.getMap()
+      const { zoom } = viewport
+
+      utils.flyToCoords(
+        map,
+        {
+          ...viewport,
+          zoom: actionID === 'in' ? zoom + 1 : zoom - 1,
+          disregardCurrZoom: true,
+        },
+        [0, 0],
+        selFeatAttribs
+      )
     }
-
-    const { zoom } = viewport
-
-    utils.flyToCoords(
-      map,
-      {
-        ...viewport,
-        zoom: actionID === 'in' ? zoom + 1 : zoom - 1,
-        disregardCurrZoom: true,
-      },
-      [0, 0]
-    )
   }
 
   return (

src/components/map/types.ts
@@ -109,7 +109,8 @@ export type FlyToCoords = (
     zoom?: number | 10.25
     disregardCurrZoom?: boolean // e.g. when using map controls
   } & LongLat,
-  offset: [number, number]
+  offset: [number, number],
+  selFeatAttribs: LangRecordSchema | null
 ) => void
 
 export type UseStyleProps = {

src/components/map/utils.ts
@@ -34,7 +34,12 @@ export const prepMapOffset = (
   return [(sidePanelWidth + sidePanelGutter) / 2, topBarHeight / 2]
 }
 
-export const flyToCoords: MapTypes.FlyToCoords = (map, settings, offset) => {
+export const flyToCoords: MapTypes.FlyToCoords = (
+  map,
+  settings,
+  offset,
+  selFeatAttribs
+) => {
   const {
     zoom: targetZoom,
     latitude: lat,
@@ -43,8 +48,9 @@ export const flyToCoords: MapTypes.FlyToCoords = (map, settings, offset) => {
   } = settings
   const currentZoom = map.getZoom()
   const customEventData = {
-    disregardCurrZoom,
     forceViewportUpdate: true, // to keep state in sync
+    selFeatAttribs, // popup data
+    disregardCurrZoom,
   }
   let zoomToUse = targetZoom
 