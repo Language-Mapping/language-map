cb8c7cbcf89493a72ae8bc9b8b43f7fc27c08388
Jason Lampel <jason@abettermap.com>
2020-09-21 11:06:34 -0600

    Table: transition via slide instead of fade

--- src/components/results/ResultsModal.tsx
+++ src/components/results/ResultsModal.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useContext, useEffect, useState } from 'react'
-import { Dialog } from '@material-ui/core'
+import { Dialog, Slide } from '@material-ui/core'
+import { TransitionProps } from '@material-ui/core/transitions'
 
 import { GlobalContext } from 'components'
 import { useStyles } from './styles'
@@ -8,6 +9,15 @@ import { ResultsTable } from './ResultsTable'
 import { LangRecordSchema } from '../../context/types'
 
 type ResultsModalProps = CloseTableProps & { open: boolean }
+
+const Transition = React.forwardRef(function Transition(
+  // Don't care, came straight from the MUI example
+  // eslint-disable-next-line react/require-default-props, @typescript-eslint/no-explicit-any
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />
+})
 
 export const ResultsModal: FC<ResultsModalProps> = (props) => {
   const { open, closeTable } = props
@@ -32,6 +42,7 @@ export const ResultsModal: FC<ResultsModalProps> = (props) => {
     <Dialog
       open={open}
       keepMounted
+      TransitionComponent={Transition}
       disableBackdropClick
       disableEscapeKeyDown
       className={`${classes.resultsModalRoot}`}
