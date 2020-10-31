a38b3cba91b243883ffbeb77e3c3f910bec27a49
Jason Lampel <jason@abettermap.com>
2020-10-31 13:54:04 -0600

    "Explore": rm list/grid toggle (list not very useful here)

--- src/components/sift/Categories.tsx
+++ src/components/sift/Categories.tsx
@@ -2,16 +2,12 @@ import React, { FC, useContext, useState, useEffect } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
-import ToggleButton from '@material-ui/lab/ToggleButton'
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup'
-import { MdViewList, MdViewModule } from 'react-icons/md'
 
 import { GlobalContext } from 'components'
 import { Category } from './Category'
 import * as Types from './types'
 import * as utils from './utils'
 
-type ViewType = 'grid' | 'list'
 const categoriesConfig = [
   { name: 'Language', summary: 'The basis of all life' },
   { name: 'Endonym', summary: 'Like Language but not' },
@@ -32,13 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
     categoriesList: {
       display: 'grid',
       gridTemplateRows: '1fr',
-      gridTemplateColumns: (props: { viewType: ViewType }) => {
-        if (props.viewType === 'grid') {
-          return '1fr 1fr'
-        }
-
-        return '1fr'
-      },
+      gridTemplateColumns: '1fr 1fr',
       gridGap: '0.65em',
     },
   })
@@ -49,8 +39,7 @@ export const Categories: FC = () => {
   const { state } = useContext(GlobalContext)
   const { langFeatsLenCache, langFeatures } = state
   const [categories, setCategories] = useState<Types.CategoryConfig[]>()
-  const [listView, setListView] = useState<ViewType>('grid')
-  const classes = useStyles({ viewType: listView })
+  const classes = useStyles()
   const { categoriesList } = classes
 
   // Prep categories
@@ -70,13 +59,6 @@ export const Categories: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [langFeatsLenCache])
 
-  const handleChange = (
-    event: React.MouseEvent<HTMLElement>,
-    nextView: 'grid' | 'list'
-  ) => {
-    setListView(nextView)
-  }
-
   return (
     <div className={classes.categoriesRoot}>
       {/* TODO: reuse and make dynamic */}
@@ -87,19 +69,6 @@ export const Categories: FC = () => {
         This is a list of all the categories (aka columns, fields) we'd want to
         let the user explore through a drill-down hierachy.
       </Typography>
-      <ToggleButtonGroup
-        value={listView}
-        size="small"
-        exclusive
-        onChange={handleChange}
-      >
-        <ToggleButton value="grid" aria-label="grid">
-          <MdViewModule />
-        </ToggleButton>
-        <ToggleButton value="list" aria-label="list">
-          <MdViewList />
-        </ToggleButton>
-      </ToggleButtonGroup>
       <div className={categoriesList}>
         {categoriesConfig &&
           categories &&
