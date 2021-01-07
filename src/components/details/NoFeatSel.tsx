97fad40b3d1a7653ac35e418410aa5e7f69a5ff6
Jason Lampel <jason@abettermap.com>
2021-01-06 20:14:45 -0700

    RandomLinkBtn: increase chance that a matching ID is found (again)

--- src/components/details/NoFeatSel.tsx
+++ src/components/details/NoFeatSel.tsx
@@ -1,39 +1,8 @@
 import React, { FC } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
-import { Typography, Button } from '@material-ui/core'
-import { FaRandom } from 'react-icons/fa'
+import { Typography } from '@material-ui/core'
 
-import { paths as routes } from 'components/config/routes'
 import { useStyles } from './styles'
-
-const RandomLinkBtn: FC = () => {
-  // const { state } = useContext(GlobalContext)
-  // const { langFeatures } = state
-
-  // if (!langFeatures.length) return null
-
-  // FIXME: all this garbage. Will likely at least have an array of `id` values
-  // in global state, so should be able to use that. ACTUALLY, only need the
-  // length of that, so can just dispatch the length somehow? Still seems hacky
-  // and overkill to use state...
-
-  // const randoIndex = Math.floor(Math.random() * (langFeatures.length - 1))
-  const randoIndex = Math.floor(Math.random() * 1000) // 😱
-  // const id = langFeatures[randoIndex].id
-
-  return (
-    <Button
-      variant="contained"
-      color="primary"
-      component={RouterLink}
-      size="small"
-      startIcon={<FaRandom />}
-      to={`${routes.details}/${randoIndex}`}
-    >
-      Try one at random
-    </Button>
-  )
-}
+import { RandomLinkBtn } from './RandomLinkBtn'
 
 export const NoFeatSel: FC<{ reason?: string }> = (props) => {
   const { reason = 'No community selected.' } = props
