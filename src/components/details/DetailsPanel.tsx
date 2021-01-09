Language-Mapping/language-map

commit cd9b9c7d905f31fd5464c29a4120a4f70ad7c850

Big TS cleanup: replace all LangRecordSchema and DetailsSchema:
...with InstanceLevelSchema. Not 100% accurate for some other fixes, but
much improvement over the three-type mess.

src/components/details/DetailsPanel.tsx
@@ -8,7 +8,7 @@ import { PanelContentSimple } from 'components/panels/PanelContent'
 import { LoadingIndicatorPanel } from 'components/generic/modals'
 import { routes } from 'components/config/api'
 import { NoFeatSel } from './NoFeatSel'
-import { WithLangDescrip } from './types'
+import { DetailsProps } from './types'
 import { useDetails } from './hooks'
 
 export const DetailsError: FC = () => {
@@ -51,10 +51,12 @@ const DetailsWrap: FC = () => {
 }
 
 // Used in /details/:id and /table/:id
-export const Details: FC<{ data: WithLangDescrip | null }> = ({ data }) => {
+export const Details: FC<DetailsProps> = (props) => {
+  const { instanceDescripID, langDescripID, data } = props
+
   if (!data) return null
 
-  const { instanceDescripID, langDescripID, Language } = data
+  const { Language } = data
   const descripID = instanceDescripID || langDescripID
 
   document.title = `${Language} - NYC Languages`

src/components/context/types.ts
@@ -5,7 +5,7 @@
 import { CensusScope } from 'components/local/types'
 
 export type PanelState = 'default' | 'maximized' | 'minimized'
-export type LangSchemaCol = keyof LangRecordSchema
+export type LangSchemaCol = keyof InstanceLevelSchema
 
 export type StoreAction =
   | { type: 'CLEAR_FILTERS'; payload: number }
@@ -114,10 +114,6 @@ export type MapToolsDispatch = React.Dispatch<MapToolsAction>
 // Fields in the actual Airtable tables
 export type LangLevelSchema = LangLevelOptional & LangLevelReqd
 export type InstanceLevelSchema = InstanceLevelOptional & InstanceLevelReqd
-
-// Dupes to avoid find-and-replace of originals
-export type LangRecordSchema = InstanceLevelSchema
-export type DetailsSchema = InstanceLevelSchema
 
 // Corresponds with top-level "/Explore" route starting points. Used as a route
 // parameter in Airtable queries in nested views, e.g. /Explore/Country/Egypt.

src/components/details/DetailedIntro.tsx
@@ -19,9 +19,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
 // The intro section of pre-Details and Details views
 export const DetailedIntro: FC<DetailedIntroProps> = (props) => {
-  const { data, shareNoun, isInstance } = props
+  const { data, shareNoun, isInstance, langDescripID } = props
   const classes = useStyles()
-  const { langDescripID } = data
 
   return (
     <header className={classes.root}>

src/components/details/hooks.ts
@@ -34,7 +34,7 @@ export const useDetails = (paramsField = 'id'): UseDetails => {
   const params = useParams() as { [key: string]: string }
   const param = params[paramsField]
 
-  const { data, isLoading, error } = useAirtable('Data', {
+  const { data, isLoading, error } = useAirtable<InstanceLevelSchema>('Data', {
     fields: dataFields,
     filterByFormula: `{id} = ${param}`,
     maxRecords: 1,
@@ -46,7 +46,7 @@ export const useDetails = (paramsField = 'id'): UseDetails => {
     data: langData,
     isLoading: isLangLoading,
     error: langError,
-  } = useAirtable(
+  } = useAirtable<LangLevelSchema>(
     'Language',
     {
       fields: langFields,
@@ -63,8 +63,6 @@ export const useDetails = (paramsField = 'id'): UseDetails => {
     theData = {
       ...langLevel,
       ...instanceLevel,
-      instanceDescripID: instanceLevel?.descriptionID,
-      langDescripID: langLevel?.descriptionID,
       Neighborhood: instanceLevel?.Neighborhood,
       Town: instanceLevel?.Town,
     }
@@ -76,5 +74,7 @@ export const useDetails = (paramsField = 'id'): UseDetails => {
     data: theData,
     isLoading: isLoading || isLangLoading,
     notFound: instanceLevel === undefined,
+    instanceDescripID: instanceLevel?.descriptionID,
+    langDescripID: langLevel?.descriptionID,
   }
 }

src/components/details/types.ts
@@ -1,7 +1,11 @@
-import { DetailsSchema } from 'components/context'
+import { InstanceLevelSchema, LangLevelSchema } from 'components/context'
+
+// FIXME: the usage of this is not accurate and is overkill for some components
+// which only need a handful of props, but it covers the TS base in a pinch
+export type FullOnEverything = LangLevelSchema & InstanceLevelSchema
 
 export type MoreLikeThisProps = {
-  data: WithLangDescrip
+  data: FullOnEverything
   isInstance?: boolean
 }
 
@@ -13,24 +17,31 @@ export type ChipProps = {
   handleClick?: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
-export type NeighborhoodListProps = MoreLikeThisProps
-export type DetailedIntroProps = MoreLikeThisProps & { shareNoun?: string }
+export type NeighborhoodListProps = MoreLikeThisProps & {
+  data: FullOnEverything
+}
 
-// Part of useDetails Hook return values with lang/instance descrip IDs
-export type WithLangDescrip = DetailsSchema & {
+export type DetailedIntroProps = Pick<MoreLikeThisProps, 'isInstance'> & {
+  data: FullOnEverything
   langDescripID?: string
-  instanceDescripID?: string
+  shareNoun?: string
 }
 
-// TODO: UGH
-export type LangOrEndoIntroProps = Pick<MoreLikeThisProps, 'isInstance'> & {
-  data: WithLangDescrip & { name?: string }
+export type DetailsProps = Pick<MoreLikeThisProps, 'isInstance'> & {
+  data: FullOnEverything | null
+  instanceDescripID?: string
+  langDescripID?: string
+  shareNoun?: string
 }
+
+export type LangOrEndoIntroProps = DetailedIntroProps & { name?: string }
 
 export type UseDetails = {
   error: unknown
   isLoading: boolean
-  data: WithLangDescrip | null
+  data: FullOnEverything | null
   notFound?: boolean
   id: string
+  instanceDescripID?: string
+  langDescripID?: string
 }

src/components/explore/LangCardsList.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import { DetailedIntro, NeighborhoodList } from 'components/details'
 import { LoadingIndicatorPanel } from 'components/generic/modals'
 import { PanelContentSimple } from 'components/panels'
+import { FullOnEverything } from 'components/details/types'
 import { useAirtable } from './hooks'
 import { RouteMatch } from './types'
 
@@ -20,7 +21,9 @@ export const LangCardsList: FC<{ field?: string }> = (props) => {
   else
     filterByFormula = `AND(FIND("${value}", ARRAYJOIN({${field}})) != 0, {name} = "${language}")`
 
-  const { data, error, isLoading } = useAirtable('Language', {
+  // FIXME: this query definitely does NOT get all the fields from
+  // `FullOnEverything`, just gave up on TSing it
+  const { data, error, isLoading } = useAirtable<FullOnEverything>('Language', {
     filterByFormula,
   })
 

src/components/explore/hooks.tsx
@@ -45,9 +45,9 @@ export const useIcon = (
   tableName = 'Country',
   imgField = 'src_image'
 ): string => {
-  // TODO: handle errors
-  // const { data, isLoading, error } = useAirtable(tableName, {
-  const { data, isLoading } = useAirtable(tableName, {
+  const { data, isLoading, error } = useAirtable<{
+    src_image: { url: string }[]
+  }>(tableName, {
     fields: [imgField],
     filterByFormula: `{name} = "${value}"`,
   })
@@ -55,7 +55,7 @@ export const useIcon = (
   // TODO: deal w/TS
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  if (isLoading || !data || !data[0] || !data[0][imgField]) return ''
+  if (isLoading || error || !data || !data[0] || !data[0][imgField]) return ''
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore

src/components/explore/types.ts
@@ -1,6 +1,6 @@
 import {
-  DetailsSchema,
-  LangRecordSchema,
+  InstanceLevelSchema,
+  LangLevelSchema,
   RouteableTableNames,
 } from 'components/context/types'
 import { AtSymbFields } from 'components/legend/types'
@@ -27,7 +27,7 @@ export type CustomCardProps = CategoryProps & {
 }
 
 export type CategoryConfig = {
-  name: keyof LangRecordSchema
+  name: keyof InstanceLevelSchema
   definition?: string
   icon?: React.ReactNode
   parse?: boolean
@@ -52,10 +52,11 @@ export type SwatchOrFlagOrIcon = {
 }
 
 export type CensusPopoverProps = {
-  data: DetailsSchema
+  data: Pick<LangLevelSchema, 'censusPretty' | 'censusScope' | 'censusField'> &
+    Pick<InstanceLevelSchema, 'Language'>
 }
 
-export type StatsAndMetaProps = { data: Partial<DetailsSchema> }
+export type StatsAndMetaProps = { data: Partial<LangLevelSchema> }
 
 export type CurrentCrumbProps = {
   value: string
@@ -68,7 +69,7 @@ export type MidLevelExploreProps = {
 }
 
 export type AirtableOptions = {
-  // fields?: Array<Extract<keyof LangRecordSchema, string>> // REFACTOR
+  // fields?: Array<Extract<keyof InstanceLevelSchema, string>> // REFACTOR
   fields?: string[]
   filterByFormula?: string
   maxRecords?: number
@@ -87,7 +88,9 @@ type SchemaTableFields = {
   includeInTable?: boolean
 }
 
-export type TonsOfFields = DetailsSchema & AtSymbFields & SchemaTableFields
+export type TonsOfFields = InstanceLevelSchema &
+  AtSymbFields &
+  SchemaTableFields
 export type TonsWithAddl = TonsOfFields & { 'Additional Languages'?: string[] }
 export type AirtableError = {
   error: string // error type, e.g. UNKNOWN_FIELD_NAME
@@ -96,7 +99,10 @@ export type AirtableError = {
 }
 
 // Details' current crumb fields which come back from Airtable
-export type CrumbResponse = Pick<DetailsSchema, 'Language' | 'Primary Location'>
+export type CrumbResponse = Pick<
+  InstanceLevelSchema,
+  'Language' | 'Primary Location'
+>
 
 export type ExploreIcon = {
   [key in RouteableTableNames]: React.ReactNode

commit 1758dd148b1dc7aed10d2352db4db0f020c0367f

Descriptions: restore functionality broken by cd9b9c7

src/components/details/DetailsPanel.tsx
@@ -36,7 +36,15 @@ export const DetailsPanel: FC = () => {
 
 // Responsible for hitting the hook and passing data
 const DetailsWrap: FC = () => {
-  const { isLoading, error, data, id, notFound } = useDetails()
+  const {
+    isLoading,
+    error,
+    instanceDescripID,
+    langDescripID,
+    data,
+    id,
+    notFound,
+  } = useDetails()
 
   if (isLoading) return <LoadingIndicatorPanel />
   if (error) return <DetailsError />
@@ -45,7 +53,7 @@ const DetailsWrap: FC = () => {
 
   return (
     <PanelContentSimple>
-      <Details data={data} />
+      <Details {...{ instanceDescripID, langDescripID, data }} />
     </PanelContentSimple>
   )
 }
@@ -63,7 +71,7 @@ export const Details: FC<DetailsProps> = (props) => {
 
   return (
     <>
-      <DetailedIntro data={data} isInstance />
+      <DetailedIntro data={data} langDescripID={langDescripID} isInstance />
       {/* There should always be a description, but checking just in case */}
       {descripID && (
         <Typography variant="body2" component="div" align="left">