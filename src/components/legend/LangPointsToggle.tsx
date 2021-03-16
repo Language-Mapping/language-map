3b2fb0041133732a19665010324e7c892f679969
Jason Lampel <jason@abettermap.com>
2021-03-16 13:05:37 -0500

    Add toggle to show/hide language points independently of labels (#187)
    Still need styling and cleanup but works like a champ.

---
+++ src/components/legend/LangPointsToggle.tsx
@@ -0,0 +1,65 @@
+// TODO: rename file to something logical; mv it and all children to ../local
+import React, { FC } from 'react'
+import { FormControlLabel, Switch } from '@material-ui/core'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+
+import { useLabelAndSymbDispatch } from 'components/context/SymbAndLabelContext'
+import { SimplePopover } from 'components/generic'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginLeft: 0,
+    },
+    controlLabel: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    smallerText: {
+      fontSize: '0.75rem',
+    },
+  })
+)
+
+export const LangPointsToggle: FC<{ checked: boolean }> = (props) => {
+  const { checked } = props
+  const classes = useStyles()
+  const { smallerText, root } = classes
+  const symbLabelDispatch = useLabelAndSymbDispatch()
+
+  const handleBoundariesToggle = () => {
+    symbLabelDispatch({
+      type: 'TOGGLE_LANG_POINTS',
+    })
+  }
+
+  const ControlLabel = (
+    <div className={classes.controlLabel}>
+      Hide language point symbols
+      <SimplePopover
+        text={
+          <>
+            Turn this on to hide the language points or symbols, which can be
+            useful if you only want to see their labels.
+          </>
+        }
+      />
+    </div>
+  )
+
+  return (
+    <FormControlLabel
+      classes={{ label: smallerText, root }}
+      style={{ marginTop: '0.5rem' }}
+      control={
+        <Switch
+          checked={checked}
+          onChange={handleBoundariesToggle}
+          name="toggle-lang-points"
+          size="small"
+        />
+      }
+      label={ControlLabel}
+    />
+  )
+}
