47878526adb7bd87e0841a8f71dbb56e6d7f7b7a
Jason Lampel <jason@abettermap.com>
2021-04-01 11:52:41 -0500

    MD conversion: allow root elem override (closes #228 freal)

--- src/components/generic/MarkdownWithRouteLinks.tsx
+++ src/components/generic/MarkdownWithRouteLinks.tsx
@@ -2,25 +2,34 @@ import React, { FC } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Link as RouterLink } from 'react-router-dom'
 
-import { LinkRenderer } from './types'
+import { LinkRenderer, MarkdownRootElemType } from './types'
 
 /* eslint-disable react/display-name */
-const renderers = {
+const renderers = (rootElemType: MarkdownRootElemType = 'span') => ({
   // ReactMarkdown ALWAYS ALWAYS ALWAYS WRAPS IN A PARAGRAPH 🤬
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  paragraph: ({ children }) => <span>{children}</span>, // TODO: UGHHHHHHHHHH
+  paragraph: ({ children }) => {
+    if (rootElemType === 'p') return <p>{children}</p>
+    if (rootElemType === 'div') return <div>{children}</div>
+
+    return <span>{children}</span>
+  }, // TODO: UGHHHHHHHHHH
   link: ({ href, node }: LinkRenderer) => (
     // Airtable converts relative links to absolute by adding the protocol 😠
     <RouterLink to={href.replace('http://', '')}>
       {node.children[0].value}
     </RouterLink>
   ),
-}
+})
 /* eslint-enable react/display-name */
 
-export const MarkdownWithRouteLinks: FC<{ text: string }> = (props) => {
-  const { text } = props
+export const MarkdownWithRouteLinks: FC<{
+  text: string
+  rootElemType?: MarkdownRootElemType
+}> = (props) => {
+  const { text, rootElemType } = props
+  const renderElems = renderers(rootElemType)
 
-  return <ReactMarkdown renderers={renderers}>{text}</ReactMarkdown>
+  return <ReactMarkdown renderers={renderElems}>{text}</ReactMarkdown>
 }
