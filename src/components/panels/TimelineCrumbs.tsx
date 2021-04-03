Language-Mapping/language-map

commit f478babb4221f93c289cb2d2c1b678e1747fd75e

Census: zoom to clicked poly, improve popups, clean config, no "Back"

src/components/panels/TimelineCrumbs.tsx
@@ -39,60 +39,53 @@ const useStyles = makeStyles((theme: Theme) =>
 export const TimelineCrumbs: FC<TimelineCrumbsProps> = (props) => {
   const classes = useStyles()
   const { pathChunks } = props
-  // 3 levels deep worth of routing looks ridiculous, esp. if no views for each:
-  const exclude = ['Census'].includes(pathChunks[0])
 
   return (
     <Timeline className={classes.root}>
-      {[...(exclude ? [pathChunks[0]] : pathChunks)]
-        .reverse()
-        .map((chunk, i) => {
-          const firstOne = i === 0
-          const panelIcon =
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            icons[chunk]
-          const to = `/${pathChunks.slice(0, pathChunks.length - i).join('/')}`
-          const emptyIconClass = panelIcon ? '' : classes.wideEmptyIcon
+      {pathChunks.reverse().map((chunk, i) => {
+        const firstOne = i === 0
+        const panelIcon =
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          icons[chunk]
+        const to = `/${pathChunks.slice(0, pathChunks.length - i).join('/')}`
+        const emptyIconClass = panelIcon ? '' : classes.wideEmptyIcon
 
-          return (
-            <TimelineItem
-              key={chunk as string}
-              classes={{ root: classes.timelineItem }}
-            >
-              <TimelineSeparator>
-                <TimelineDot
-                  color="secondary"
-                  variant={firstOne ? 'outlined' : 'default'}
-                  className={emptyIconClass}
-                >
-                  {panelIcon}
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent>
-                {firstOne ? (
-                  <b>
-                    <Switch>
-                      <Route
-                        path="/Explore/Language/:language/:instanceID"
-                        exact
-                      >
-                        Community details
-                      </Route>
-                      <Route path="/Explore/Language/none">
-                        No community selected
-                      </Route>
-                      <Route>{chunk}</Route>
-                    </Switch>
-                  </b>
-                ) : (
-                  <RouterLink to={to}>{chunk}</RouterLink>
-                )}
-              </TimelineContent>
-            </TimelineItem>
-          )
-        })}
+        return (
+          <TimelineItem
+            key={chunk as string}
+            classes={{ root: classes.timelineItem }}
+          >
+            <TimelineSeparator>
+              <TimelineDot
+                color="secondary"
+                variant={firstOne ? 'outlined' : 'default'}
+                className={emptyIconClass}
+              >
+                {panelIcon}
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              {firstOne ? (
+                <b>
+                  <Switch>
+                    <Route path="/Explore/Language/:language/:instanceID" exact>
+                      Community details
+                    </Route>
+                    <Route path="/Explore/Language/none">
+                      No community selected
+                    </Route>
+                    <Route>{chunk}</Route>
+                  </Switch>
+                </b>
+              ) : (
+                <RouterLink to={to}>{chunk}</RouterLink>
+              )}
+            </TimelineContent>
+          </TimelineItem>
+        )
+      })}
       {/* Always link to Home at bottom of timeline */}
       <TimelineItem classes={{ root: classes.timelineItem }}>
         <TimelineSeparator>

src/components/map/CensusLayer.tsx
@@ -1,39 +1,61 @@
 import React, { FC } from 'react'
 import { Source, Layer } from 'react-map-gl'
 
-import { CensusScope } from 'components/local/types'
 import { CensusLayerProps } from './types'
-import { useCensusSymb } from './hooks'
-import { CENSUS_PROMOTE_ID_FIELD } from './config'
+import { useCensusSymb, useZoomToBounds } from './hooks'
+import { CENSUS_PROMOTE_ID_FIELD, censusLayersConfig } from './config'
 
 export const CensusLayer: FC<CensusLayerProps> = (props) => {
-  const { sourceLayer, config, map, beforeId } = props
-  const { layers, source } = config
-  const censusScope = config.source.id as CensusScope
+  const { map, beforeId, configKey, mapLoaded } = props
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore // TODO: come on
+  const layerConfig = censusLayersConfig[configKey]
+  const { sourceID, sourceLayer, routePath } = layerConfig
+  const { tableName, url, linePaint } = layerConfig
+
   const { fillPaint, visible, error, isLoading } = useCensusSymb(
     sourceLayer,
-    censusScope,
+    sourceID,
     map
   )
+
+  useZoomToBounds(routePath, tableName, mapLoaded, map)
 
   if (error || isLoading) return null // TODO: sentry
 
   return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    <Source {...source} type="vector" promoteId={CENSUS_PROMOTE_ID_FIELD}>
-      {layers.map((layer) => (
-        <Layer
-          key={layer.id}
-          beforeId={beforeId}
-          {...layer}
-          paint={layer.type === 'line' ? layer.paint : fillPaint}
-          layout={{
-            ...layer.layout,
-            visibility: visible ? 'visible' : 'none',
-          }}
-        />
-      ))}
+    <Source
+      id={sourceID}
+      url={url}
+      type="vector"
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      promoteId={CENSUS_PROMOTE_ID_FIELD}
+    >
+      <Layer
+        // TODO: rm if layer order is never important
+        id={`${sourceID}-placeholder`}
+        type="background"
+        paint={{ 'background-opacity': 0 }}
+      />
+      <Layer
+        beforeId={beforeId}
+        id={`${sourceID}-poly`}
+        source={sourceID}
+        source-layer={sourceLayer}
+        type="fill"
+        paint={fillPaint}
+        layout={{ visibility: visible ? 'visible' : 'none' }}
+      />
+      <Layer
+        beforeId={beforeId}
+        id={`${sourceID}-line`}
+        source={sourceID}
+        source-layer={sourceLayer}
+        type="line"
+        paint={linePaint}
+        layout={{ visibility: visible ? 'visible' : 'none' }}
+      />
     </Source>
   )
 }

src/components/map/Map.tsx
@@ -20,7 +20,12 @@ import { CensusLayer } from './CensusLayer'
 import { GeocodeMarker } from './GeocodeMarker'
 import { PolygonLayer } from './NeighborhoodsLayer'
 
-import * as config from './config'
+import {
+  initialMapState,
+  mapProps,
+  POINT_ZOOM_LEVEL,
+  langTypeIconsConfig,
+} from './config'
 import {
   useSelLangPointCoords,
   useOffset,
@@ -34,8 +39,6 @@ import { onHover } from './events'
 
 import * as Types from './types'
 import * as utils from './utils'
-
-const { langTypeIconsConfig } = config
 
 utils.rightToLeftSetup()
 
@@ -64,9 +67,7 @@ export const Map: FC<Types.MapProps> = (props) => {
     geocodeMarker,
     setGeocodeMarker,
   ] = useState<Types.GeocodeMarkerProps | null>()
-  const [viewport, setViewport] = useState<Types.ViewportState>(
-    config.initialMapState
-  )
+  const [viewport, setViewport] = useState<Types.ViewportState>(initialMapState)
 
   // TODO: mobile:
   const shouldFlyHome = useZoomToLangFeatsExtent(isMapTilted, map)
@@ -249,6 +250,7 @@ export const Map: FC<Types.MapProps> = (props) => {
 
     let targetRoute = ''
 
+    // TODO: tighten up routes w/TS, and move most of this function into utils
     if (clickedPolygons.length) {
       const topPoly = clickedPolygons[0]
       const { properties, source } = topPoly
@@ -289,7 +291,7 @@ export const Map: FC<Types.MapProps> = (props) => {
     <>
       <MapGL
         {...viewport}
-        {...config.mapProps}
+        {...mapProps}
         ref={mapRef}
         /* eslint-disable operator-linebreak */
         // interactiveLayerIds={
@@ -311,7 +313,7 @@ export const Map: FC<Types.MapProps> = (props) => {
           onViewportChange={(mapViewport: Types.ViewportState) => {
             // CRED:
             // github.com/visgl/react-map-gl/issues/887#issuecomment-531580394
-            setViewport({ ...mapViewport, zoom: config.POINT_ZOOM_LEVEL })
+            setViewport({ ...mapViewport, zoom: POINT_ZOOM_LEVEL })
           }}
         />
         {/* TODO: hide label on clear */}
@@ -330,15 +332,15 @@ export const Map: FC<Types.MapProps> = (props) => {
         />
         <CensusLayer
           map={map}
-          config={config.pumaConfig}
+          configKey="puma"
           beforeId={beforeId}
-          sourceLayer={config.pumaLyrSrc['source-layer']}
+          mapLoaded={mapLoaded}
         />
         <CensusLayer
           map={map}
-          config={config.tractConfig}
+          configKey="tract"
           beforeId={beforeId}
-          sourceLayer={config.tractLyrSrc['source-layer']}
+          mapLoaded={mapLoaded}
         />
         <LangMbSrcAndLayer />
         {mapLoaded && showPopups && !mapIsMoving && (

src/components/map/MapPopup.tsx
@@ -151,14 +151,14 @@ const CensusPopup: FC<MapPopupsProps> = (props) => {
 
   if (isLoading || error || !data.length) return <></>
 
-  const firstResult = data[0]
-  const { latitude, longitude } = getCenterOfBounds(firstResult)
-  const heading = `${firstResult[field]} speakers`
+  const firstRow = data[0]
+  const { latitude, longitude } = getCenterOfBounds(firstRow)
+  const heading = firstRow[field] ? firstRow[field].toLocaleString() : 'No'
 
   const Content = (
     <>
       of <i>{censusActiveField?.pretty || field}</i> in{' '}
-      {`${firstResult.Neighborhood || 'this census tract'}`}
+      {`${firstRow.Neighborhood || 'this census tract'}`}
     </>
   )
 
@@ -167,7 +167,7 @@ const CensusPopup: FC<MapPopupsProps> = (props) => {
       longitude={longitude}
       latitude={latitude}
       setShowPopups={setShowPopups}
-      heading={heading}
+      heading={`${heading} speakers`}
       content={Content}
     />
   )

src/components/map/NeighborhoodsLayer.tsx
@@ -3,9 +3,8 @@ import { useRouteMatch } from 'react-router-dom'
 import { Source, Layer } from 'react-map-gl'
 
 import { useMapToolsState } from 'components/context'
-import { PolygonLayerProps, BoundsArray } from './types'
-import { usePolygonWebMerc, useOffset } from './hooks'
-import { getPolyWebMercView, flyToPoint } from './utils'
+import { PolygonLayerProps } from './types'
+import { useZoomToBounds } from './hooks'
 
 const polygonsConfigNew = {
   neighborhoods: {
@@ -71,9 +70,8 @@ export const PolygonLayer: FC<PolygonLayerProps> = (props) => {
     exact: true,
   })
   const valueParam = match?.params.name
-  const selPolyBounds = usePolygonWebMerc(routePath, tableName)
-  const offset = useOffset()
-  const { x_max: xMax, x_min: xMin, y_min: yMin, y_max: yMax } = selPolyBounds
+
+  useZoomToBounds(routePath, tableName, mapLoaded, map)
 
   // Clear/set selected feature state
   useEffect(() => {
@@ -94,27 +92,6 @@ export const PolygonLayer: FC<PolygonLayerProps> = (props) => {
     // Definitely need `mapLoaded`
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [map, mapLoaded, match, valueParam, visible])
-
-  // Zoom to selected feature extent
-  useEffect(() => {
-    if (!map || !mapLoaded || !xMax || !xMin || !yMin || !yMax) return
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const boundsArray = [
-      [xMin, yMin],
-      [xMax, yMax],
-    ] as BoundsArray
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const webMercViewport = getPolyWebMercView(boundsArray, offset)
-
-    flyToPoint(map, { ...webMercViewport, offset })
-
-    // LEGIT. selPolyBounds as a dep will break the world.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mapLoaded, xMax, xMin, yMin, yMax, map])
 
   return (
     <Source

src/components/map/config.census.ts
@@ -1,28 +1,9 @@
-import * as LocalTypes from 'components/local'
-import * as Types from './types'
-
-const tractSrcId: LocalTypes.CensusScope = 'tract'
-const pumaSrcID: LocalTypes.CensusScope = 'puma'
-const minzoom = 8
-
 // Tell MB not to use default `id` as unique ID
 export const CENSUS_PROMOTE_ID_FIELD = 'GEOID'
 
-export const pumaLyrSrc = {
-  source: pumaSrcID,
-  'source-layer': pumaSrcID,
-  minzoom,
-}
-
-export const tractLyrSrc = {
-  source: tractSrcId,
-  'source-layer': tractSrcId,
-  minzoom,
-}
-
 // The feature-state approach came from:
 // https://docs.mapbox.com/help/tutorials/data-joins-with-mapbox-boundaries/
-const censusFill = {
+const fillPaint = {
   type: 'fill',
   paint: {
     'fill-color': [
@@ -43,29 +24,27 @@ const censusFill = {
   },
 }
 
-const censusLine = {
-  type: 'line',
-  paint: { 'line-color': '#c2c2c2', 'line-opacity': 0.2 },
-}
+const linePaint = { 'line-color': '#c2c2c2', 'line-opacity': 0.2 }
 
-export const tractConfig = {
-  source: {
-    id: tractSrcId,
-    url: 'mapbox://elalliance.ckmz05zuf04p421nltfeih779-91sof',
-  },
-  layers: [
-    { id: 'tract-poly', ...tractLyrSrc, ...censusFill },
-    { id: 'tract-line', ...tractLyrSrc, ...censusLine },
-  ],
-} as Omit<Types.BoundaryConfig, 'lookupPath'>
-
-export const pumaConfig = {
-  source: {
-    id: pumaSrcID,
+export const censusLayersConfig = {
+  puma: {
+    linePaint,
+    routePath: '/Census/puma/:field/:id',
+    sourceID: 'puma',
+    sourceLayer: 'puma',
+    tableName: 'puma',
     url: 'mapbox://elalliance.ckmyzhyit0n4b21mxt9rdkeiy-7w6dn',
+    visContextKey: 'showNeighbs',
+    fillPaint,
   },
-  layers: [
-    { id: 'puma-poly', ...pumaLyrSrc, ...censusFill },
-    { id: 'puma-line', ...pumaLyrSrc, ...censusLine },
-  ],
-} as Omit<Types.BoundaryConfig, 'lookupPath'>
+  tract: {
+    linePaint,
+    routePath: '/Census/tract/:field/:id',
+    sourceID: 'tract',
+    sourceLayer: 'tract',
+    tableName: 'tract',
+    url: 'mapbox://elalliance.ckmz05zuf04p421nltfeih779-91sof',
+    visContextKey: 'showCounties',
+    fillPaint,
+  },
+}

src/components/map/hooks.tsx
@@ -155,10 +155,13 @@ export const usePolygonWebMerc: Types.UsePolygonWebMerc = (
   routePath,
   tableName
 ) => {
-  const match = useRouteMatch<{ name: string }>({
+  const match = useRouteMatch<{ name?: string; id?: string }>({
     path: routePath,
     exact: true,
   })
+  const isCensus = ['puma', 'tract'].includes(tableName)
+  const filterField = isCensus ? 'GEOID' : 'name'
+  const filterValue = match?.params.name || match?.params.id
 
   const { data, isLoading, error } = useAirtable<
     Types.NeighborhoodTableSchema | Types.CountyTableSchema
@@ -166,8 +169,9 @@ export const usePolygonWebMerc: Types.UsePolygonWebMerc = (
     tableName,
     {
       fields: ['x_max', 'x_min', 'y_min', 'y_max'],
-      filterByFormula: `{name} = "${match?.params.name}"`,
+      filterByFormula: `{${filterField}} = "${filterValue}"`,
       maxRecords: 1,
+      ...(isCensus && { baseID: AIRTABLE_CENSUS_BASE }),
     },
     { enabled: !!match }
   )
@@ -300,4 +304,33 @@ export const useCensusSymb: Types.UseCensusSymb = (
   }, [highLow])
 
   return { fillPaint, visible, error, isLoading }
+}
+
+export const useZoomToBounds: Types.UseZoomToBounds = (
+  routePath,
+  tableName,
+  mapLoaded,
+  map
+) => {
+  const selPolyBounds = usePolygonWebMerc(routePath, tableName)
+  const { x_max: xMax, x_min: xMin, y_min: yMin, y_max: yMax } = selPolyBounds
+  const offset = useOffset()
+
+  // Zoom to selected feature extent
+  useEffect(() => {
+    if (!map || !mapLoaded || !xMax || !xMin || !yMin || !yMax) return
+
+    const boundsArray = [
+      [xMin, yMin],
+      [xMax, yMax],
+    ] as Types.BoundsArray
+
+    const webMercViewport = utils.getPolyWebMercView(boundsArray, offset)
+    const zoom = Math.min(webMercViewport.zoom, 13) // tracts are too small
+
+    flyToPoint(map, { ...webMercViewport, offset, zoom })
+
+    // LEGIT. selPolyBounds as a dep will break the world.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mapLoaded, xMax, xMin, yMin, yMax, map])
 }

src/components/map/types.ts
@@ -128,8 +128,8 @@ export type PanelSectionProps = {
 }
 
 export type CensusLayerProps = {
-  sourceLayer: string
-  config: Omit<BoundaryConfig, 'lookupPath'>
+  mapLoaded: boolean
+  configKey: string
   beforeId?: string
   map?: Map
 }
@@ -306,3 +306,10 @@ export type NeighborhoodTableSchema = BoundsColumns & {
 export type CountyTableSchema = BoundsColumns & {
   name: string
 }
+
+export type UseZoomToBounds = (
+  routePath: string,
+  tableName: string,
+  mapLoaded: boolean,
+  map?: Map
+) => void

commit 8b09a851fc64d49aecef5851e6656a3ec50d1943

"Timeline" nav: undo the breakage caused by f478bab

src/components/panels/TimelineCrumbs.tsx
@@ -42,7 +42,7 @@ export const TimelineCrumbs: FC<TimelineCrumbsProps> = (props) => {
 
   return (
     <Timeline className={classes.root}>
-      {pathChunks.reverse().map((chunk, i) => {
+      {[...pathChunks].reverse().map((chunk, i) => {
         const firstOne = i === 0
         const panelIcon =
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment