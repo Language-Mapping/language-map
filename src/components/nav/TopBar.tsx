b86100ed6f56e7c33bf560955622099d9d78fbe5
Jason Lampel <jason@abettermap.com>
2021-04-05 18:47:47 -0500

    Main logo: use white text if dark baselayer selected

--- src/components/nav/TopBar.tsx
+++ src/components/nav/TopBar.tsx
@@ -4,21 +4,23 @@ import { Typography } from '@material-ui/core'
 
 import { routes } from 'components/config/api'
 import { usePanelState } from 'components/panels'
-import { ReactComponent as Logo } from '../../img/logo.svg'
+import { useMapToolsState } from 'components/context'
+import { Logo } from 'components/generic'
 import { PAGE_HEADER_ID } from './config'
 import { useStyles } from './styles'
 
 export const TopBar: FC = () => {
   const { panelOpen } = usePanelState()
+  const { baseLayer } = useMapToolsState()
   const classes = useStyles({ panelOpen })
-  const { spacerDesktop, spacerLeft, title, root, logo } = classes
+  const { spacerDesktop, spacerLeft, title, root } = classes
 
   return (
     <header className={root} id={PAGE_HEADER_ID}>
       <div className={`${spacerDesktop} ${spacerLeft}`} />
       <Typography variant="h2" component="h1" className={title}>
         <RouteLink to={routes.home} data-testid={PAGE_HEADER_ID}>
-          <Logo className={logo} />
+          <Logo darkTheme={baseLayer !== 'light'} />
         </RouteLink>
       </Typography>
       <div className={spacerDesktop} />
