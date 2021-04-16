856eabc5caf79a382daf6545cd3213198f8d5e86
Jason Lampel <jason@abettermap.com>
2021-04-16 18:47:47 -0500

    RIP: React Lazy/Suspense. No-go on slow connections :(

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
-import React, { FC, Suspense } from 'react'
+import React, { FC } from 'react'
 import * as Sentry from '@sentry/react'
 // import { ReactQueryDevtools } from 'react-query-devtools'
 import Airtable from 'airtable'
 
 import { AIRTABLE_API_KEY } from 'components/config'
 import { PanelContextProvider } from 'components/panels'
+import { ResultsModal } from 'components/results'
 import { AppWrap } from './AppWrap'
 import { MapToolsProvider } from './context/MapToolsContext'
 
@@ -22,14 +23,6 @@ import { MapToolsProvider } from './context/MapToolsContext'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore // great start 🙄
 Airtable.configure({ apiKey: AIRTABLE_API_KEY })
-
-// Good tut on this: https://ui.dev/react-router-v4-code-splitting/
-const LazyTable = React.lazy(
-  () =>
-    // webpackChunkName does not seem to work here. Maybe because there is
-    // another webpackChunkName nested inside the imported components?
-    import(/* webpackChunkName: "table" */ 'components/results/ResultsModal')
-)
 
 export const App: FC = () => {
   return (
@@ -54,10 +47,8 @@ export const App: FC = () => {
         <PanelContextProvider>
           <AppWrap />
         </PanelContextProvider>
-        {/* TODO: understand this and create legit fallback element */}
-        <Suspense fallback={<div />}>
-          <LazyTable />
-        </Suspense>
+        {/* TODO: make Suspense/Lazy work on poor connections */}
+        <ResultsModal />
       </MapToolsProvider>
       {/* <ReactQueryDevtools /> */}
     </Sentry.ErrorBoundary>
