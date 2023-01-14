48c201112485d19ddec06006aa620b65118dc02a
Jason Lampel <jason@abettermap.com>
2023-01-14 14:45:34 -0600

    WP content query: add password as query string

--- src/components/about/utils.ts
+++ src/components/about/utils.ts
@@ -4,7 +4,11 @@ import { WP_API_PAGES_ENDPOINT } from './config'
 // Define a default query function that will receive the query key
 export const defaultQueryFn = async <T extends unknown>(
   key: number
-): Promise<T> => (await fetch(`${WP_API_PAGES_ENDPOINT}/${key}`)).json()
+): Promise<T> =>
+  // `password` is part of a hack to attempt to exclude/filter NYC Map pages
+  // from the ELA website's NextJS builds. It is not intended for security, so
+  // it's safe to commit.
+  (await fetch(`${WP_API_PAGES_ENDPOINT}/${key}?password=1234`)).json()
 
 // Provide the default query function to your app with defaultConfig
 export const wpQueryCache = new QueryCache({
